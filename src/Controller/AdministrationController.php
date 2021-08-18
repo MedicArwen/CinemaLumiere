cba7dd2540cf9cacf51451657902eb8be00d0fbb
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2021-08-18 16:07:33 +0200

    ajout des templates
    ajout des templates

---
+++ src/Controller/AdministrationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\GenreService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdministrationController extends AbstractController
+{
+    /**
+     * @Route("/administration", name="administration")
+     */
+    public function index(): Response
+    {
+        return $this->render('administration/index.html.twig', [
+            'controller_name' => 'AdministrationController',
+        ]);
+    }
+    /**
+     * @Route("administration/genres",name="liste_genres")
+     */
+    public function listeGenres(GenreService $genreService):Response
+    {
+        $genres = $genreService->liste();
+        return $this->render('administration/genres/liste_genres.html.twig', [
+            'genres' => $genres,
+        ]);
+    }
+}
