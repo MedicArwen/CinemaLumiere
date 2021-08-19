473c42c5d5abe2b6ee9f4ae00240dc0054f05ae0
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2021-08-19 09:35:23 +0200

    ajout des formulaires manquants
    ajout du formulaire creer film et creer seance

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
 use App\Service\GenreService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Genre;
+use App\Entity\Seance;
+use App\Form\FilmFormType;
 use App\Form\GenreFormType;
+use App\Form\SeanceFormType;
+use App\Service\FilmService;
+use App\Service\SeanceService;
 
 class AdministrationController extends AbstractController
 {
@@ -33,11 +39,31 @@ class AdministrationController extends AbstractController
     /**
      * @Route("administration/genres/add",name="admin_creer_genre")
      */
-    public function creerGenres(GenreService $genreService):Response
+    public function creerGenres(GenreService $pGenreService):Response
     {
         $genre = new Genre();
         $formulaire = $this->createForm(GenreFormType::class,$genre);
         return $this->render('administration/genres/form_genres.html.twig', [
             'formulaire' => $formulaire->createView()]);
         }
+/**
+ * @Route("administration/films/add", name="admin_creer_film")
+ */
+        public function creerFilms(FilmService $pFilmService)
+        {
+            $film = new Film();
+            $formulaire = $this->createForm(FilmFormType::class,$film);
+            return $this->render('administration/films/form_films.html.twig',
+            ['formulaire' => $formulaire->createView()]);
+        }
+/**
+ * @Route("administration/seances/add", name="admin_creer_seance")
+ */
+        public function creerSeances(SeanceService $pSeanceService)
+    {
+        $seance = new Seance();
+        $formulaire = $this->createForm(SeanceFormType::class,$seance);
+        return $this->render('administration/seances/form_seances.html.twig',
+        ['formulaire' => $formulaire->createView()]);
+    }
 }
